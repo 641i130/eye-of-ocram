edbd86d24486ea222cecdb4c88cee1d4ac29add5
641i130 <galileoray@gmail.com>
2022-10-12 14:33:30 -0500

    CAN READ STRINGS

--- src/world.rs
+++ src/world.rs
@@ -19,15 +19,22 @@ impl World {
     }
     fn skip_bytes<R: std::io::Read>(iterator: &mut std::io::Bytes<R>,count: u8) {
         for _ in 0..count {
-            let skipped = iterator.next().expect("No more bytes!").unwrap();
+            let _ = iterator.next().expect("No more bytes!").unwrap();
         }
+    }
+    fn read_string<R: std::io::Read>(iterator: &mut std::io::Bytes<R>) -> String {
+        let mut out = vec![]; // Build string vector
+        for _ in 0..World::get_byte(iterator) { // Get string length 
+            out.push(World::get_byte(iterator) as char);
+        }
+        out.iter().cloned().collect::<String>()
     }
     pub fn read_wld(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         let mut wldp = WldReader::new(&self.file_name);
         let mut iterator = wldp.input.bytes();
         self.version = World::get_byte(&mut iterator);
-        World::skip_bytes(&mut iterator,8); // Skip 8 bytes!
-        println!("{:?}",World::get_byte(&mut iterator));
+        World::skip_bytes(&mut iterator,7); // Skip 8 bytes!
+        self.name = World::read_string(&mut iterator);
         Ok(())
     }
     pub fn pretty_print(self) {
